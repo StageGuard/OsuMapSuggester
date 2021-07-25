e23b8e3bfc70873e7eed99754aaa18430b8a8051
StageGuard <1355416608@qq.com>
2021-07-25 11:27:39 +0800

    pp calculator and reformat Mod

--- src/main/kotlin/me/stageguard/obms/algorithm/beatmap/Mod.kt
+++ src/main/kotlin/me/stageguard/obms/algorithm/beatmap/Mod.kt
@@ -1,44 +1,73 @@
+@file:Suppress("unused")
+
 package me.stageguard.obms.algorithm.beatmap
 
-class ModCombination(private val value: Int) {
-    companion object Mod {
-        const val NF: Int = 1 shl 0
-        const val EZ: Int = 1 shl 1
-        const val TD: Int = 1 shl 2
-        const val HD: Int = 1 shl 3
-        const val HR: Int = 1 shl 4
-        const val DT: Int = 1 shl 6
-        const val RX: Int = 1 shl 7
-        const val HT: Int = 1 shl 8
-        const val FL: Int = 1 shl 10
-        const val SO: Int = 1 shl 12
-        const val AP: Int = 1 shl 13
-        const val V2: Int = 1 shl 29
-    }
+class ModCombination private constructor(private val value: Int) {
 
-    fun isDoubleTimeOrHalfTime() = value and (HT or DT) > 0
-    fun isScoreMode() = value and (HT or DT or HR or EZ) > 0
+    fun isDoubleTimeOrHalfTime() = value and (Mod.HalfTime.value or Mod.DoubleTime.value) > 0
+    fun isScoreMode() = value and (Mod.HalfTime.value or Mod.DoubleTime.value or Mod.HardRock.value or Mod.Easy.value) > 0
     fun speed() = when {
-        value and DT > 0 -> 1.5
-        value and HT > 0 -> 0.75
+        value and Mod.DoubleTime.value > 0 -> 1.5
+        value and Mod.HalfTime.value > 0 -> 0.75
         else -> 1.0
     }
     fun odArHpMultiplier() = when {
-        value and HR > 0 -> 1.4
-        value and EZ > 0 -> 0.5
+        value and Mod.HardRock.value > 0 -> 1.4
+        value and Mod.Easy.value > 0 -> 0.5
         else -> 1.0
     }
 
-    fun nf() = value and NF > 0
-    fun ez() = value and EZ > 0
-    fun td() = value and TD > 0
-    fun hd() = value and HD > 0
-    fun hr() = value and HR > 0
-    fun dt() = value and DT > 0
-    fun rx() = value and RX > 0
-    fun ht() = value and HT > 0
-    fun fl() = value and FL > 0
-    fun so() = value and SO > 0
-    fun ap() = value and AP > 0
-    fun v2() = value and V2 > 0
+    fun nf() = value and Mod.NoFail.value > 0
+    fun ez() = value and Mod.Easy.value > 0
+    fun td() = value and Mod.TouchDevice.value > 0
+    fun hd() = value and Mod.Hidden.value > 0
+    fun hr() = value and Mod.HardRock.value > 0
+    fun dt() = value and Mod.DoubleTime.value > 0
+    fun rx() = value and Mod.Relax.value > 0
+    fun ht() = value and Mod.HalfTime.value > 0
+    fun fl() = value and Mod.NoFail.value > 0
+    fun so() = value and Mod.SpunOut.value > 0
+    fun ap() = value and Mod.Perfect.value > 0
+    fun v2() = value and Mod.ScoreV2.value > 0
+
+    companion object {
+        fun of(vararg mods: Mod) = ModCombination(
+            if(mods.size == 1) mods.single().value else mods.drop(1).fold(mods.first().value) { r, v -> r or v.value }
+        )
+    }
+}
+
+sealed class Mod(val value: Int) {
+    object None : Mod(0)
+    object NoFail : Mod(1)
+    object Easy : Mod(2)
+    object TouchDevice : Mod(4)
+    object Hidden : Mod(8)
+    object HardRock : Mod(16)
+    object SuddenDeath : Mod(32)
+    object DoubleTime : Mod(64)
+    object Relax : Mod(128)
+    object HalfTime : Mod(256)
+    object NightCore : Mod(512)
+    object Flashlight : Mod(1024)
+    object Autoplay : Mod(2048)
+    object SpunOut : Mod(4096)
+    object Relax2 : Mod(8192)
+    object Perfect : Mod(16384)
+    object Key4 : Mod(32768)
+    object Key5 : Mod(65536)
+    object Key6 : Mod(131072)
+    object Key7 : Mod(262144)
+    object Key8 : Mod(524288)
+    object FadeIn : Mod(1048576)
+    object Random : Mod(2097152)
+    object Cinema : Mod(4194304)
+    object Target : Mod(8388608)
+    object Key9 : Mod(16777216)
+    object KeyCoop : Mod(33554432)
+    object Key1 : Mod(67108864)
+    object Key3 : Mod(134217728)
+    object Key2 : Mod(268435456)
+    object ScoreV2 : Mod(536870912)
+    object Mirror : Mod(1073741824)
 }
--- src/main/kotlin/me/stageguard/obms/algorithm/pp/PPCalculator.kt
+++ src/main/kotlin/me/stageguard/obms/algorithm/pp/PPCalculator.kt
@@ -2,16 +2,15 @@ package me.stageguard.obms.algorithm.pp
 
 import me.stageguard.obms.algorithm.beatmap.Beatmap
 import me.stageguard.obms.algorithm.beatmap.ModCombination
+import me.stageguard.obms.algorithm.beatmap.Mod
 import java.util.*
-import kotlin.math.max
-import kotlin.math.min
-import kotlin.math.round
+import kotlin.math.*
 
 class PPCalculator private constructor(
     val beatmap: Beatmap
 ) {
     private var attributes: Optional<DifficultyAttributes> = Optional.empty()
-    private var mods: Int = 0
+    private var mods: ModCombination = ModCombination.of(Mod.None)
     private var combo: Optional<Int> = Optional.empty()
     private var acc: Optional<Double> = Optional.empty()
 
@@ -21,17 +20,22 @@ class PPCalculator private constructor(
     private var nMisses: Int = 0
     private var passedObjects: Optional<Int> = Optional.empty()
 
-    fun attributes(attr: Optional<DifficultyAttributes>) = this.also {
+    private val totalHits get() = kotlin.run {
+        val nObjects = this.passedObjects.orElse(this.beatmap.hitObjects.size)
+        min(this.n300.orElse(0) + this.n100.orElse(0) + this.n50.orElse(0) + this.nMisses, nObjects)
+    }
+
+    @Suppress("unused") fun attributes(attr: Optional<DifficultyAttributes>) = this.also {
         attr.ifPresent { this.attributes = Optional.of(it) }
     }
-    fun mods(mod: Int) = this.also { this.mods = mod }
-    fun combo(cb: Int) = this.also { this.combo = Optional.of(cb) }
-    fun n300(n : Int) = this.also { this.n300 = Optional.of(n) }
-    fun n100(n: Int) = this.also { this.n100 = Optional.of(n) }
-    fun n50(n: Int) = this.also { this.n50 = Optional.of(n) }
-    fun misses(n: Int) = this.also { this.nMisses = n }
-    fun passedObjects(n: Int) = this.also { this.passedObjects = Optional.of(n) }
-    fun accuracy(acc: Double) = this.also {
+    @Suppress("unused") fun mods(vararg mods: Mod) = this.also { this.mods = ModCombination.of(*mods) }
+    @Suppress("unused") fun combo(cb: Int) = this.also { this.combo = Optional.of(cb) }
+    @Suppress("unused") fun n300(n : Int) = this.also { this.n300 = Optional.of(n) }
+    @Suppress("unused") fun n100(n: Int) = this.also { this.n100 = Optional.of(n) }
+    @Suppress("unused") fun n50(n: Int) = this.also { this.n50 = Optional.of(n) }
+    @Suppress("unused") fun misses(n: Int) = this.also { this.nMisses = n }
+    @Suppress("unused") fun passedObjects(n: Int) = this.also { this.passedObjects = Optional.of(n) }
+    @Suppress("unused") fun accuracy(acc: Double) = this.also {
         val nObjects = this.passedObjects.orElse(beatmap.hitObjects.size)
 
         val accuracy = acc / 100.0
@@ -100,15 +104,19 @@ class PPCalculator private constructor(
             )
 
             if(remaining > 0) {
-                if (this.n300.isEmpty) {
-                    this.n300 = Optional.of(remaining)
-                } else if (this.n100.isEmpty) {
-                    this.n100 = Optional.of(remaining)
-                }
-                if (this.n50.isEmpty) {
-                    this.n50 = Optional.of(remaining)
-                } else {
-                    this.n300 = Optional.of(this.n300.get() + remaining)
+                when {
+                    this.n300.isEmpty -> {
+                        this.n300 = Optional.of(remaining)
+                    }
+                    this.n100.isEmpty -> {
+                        this.n100 = Optional.of(remaining)
+                    }
+                    this.n50.isEmpty -> {
+                        this.n50 = Optional.of(remaining)
+                    }
+                    else -> {
+                        this.n300 = Optional.of(this.n300.get() + remaining)
+                    }
                 }
             }
 
@@ -132,12 +140,156 @@ class PPCalculator private constructor(
         }
     }
 
-    fun calculate() {
+    private fun calculateAim(totalHits: Double) : Double {
+        val attributes = this.attributes.get()
+        // TD penalty
+        val rawAim = attributes.aimStrain.run {
+            if(this@PPCalculator.mods.td()) this.pow(0.8) else this
+        }
+
+        var aimValue = (5.0 * max(rawAim / 0.0675, 1.0) - 4.0).pow(3) / 100_000.0
+
+        // Longer maps are worth more
+        val lenBonus = 0.95 + 0.4 * min(totalHits / 2000.0, 1.0) +
+                (if(totalHits > 2000.0) 1.0 else 0.0) * 0.5 * log10(totalHits / 2000.0)
+        aimValue *= lenBonus
+
+        // Penalize misses
+        if (this.nMisses > 0) {
+            aimValue *= 0.97 * (1.0 - (this.nMisses / totalHits).pow(0.775)).pow(this.nMisses)
+        }
+
+        // Combo scaling
+        this.combo.filter { attributes.maxCombo > 0 }.ifPresent { combo ->
+            aimValue *= min((combo.toDouble() / attributes.maxCombo.toDouble()).pow(0.8), 1.0)
+        }
+
+        // AR bonus
+        var approachRateFactor = 0.0
+        if (attributes.approachRate > 10.33) {
+            approachRateFactor += 0.4 * (attributes.approachRate - 10.33)
+        } else if (attributes.approachRate < 8.0) {
+            approachRateFactor += 0.01 * (8.0 - attributes.approachRate)
+        }
+        aimValue *= 1.0 + min(approachRateFactor, approachRateFactor * totalHits / 1000.0)
+
+        // HD bonus
+        if (this.mods.hd()) {
+            aimValue *= 1.0 + 0.04 * (12.0 - attributes.approachRate)
+        }
+
+        // FL bonus
+        if (this.mods.fl()) {
+            aimValue *= 1.0 +
+                0.35 * min(totalHits / 200.0, 1.0) +
+                (if(totalHits > 200) 1.0 else 0.0) * 0.3 * min((totalHits - 200.0) / 300.0, 1.0) +
+                (if(totalHits > 500) 1.0 else 0.0) * (totalHits - 500.0) / 1200.0
+        }
+
+        // Scale with accuracy
+        aimValue *= 0.5 + this.acc.get() / 2.0
+        aimValue *= 0.98 + attributes.overallDifficulty * attributes.overallDifficulty / 2500.0
+
+        return aimValue
+    }
+
+    private fun calculateSpeed(totalHits: Double) : Double {
+        val attributes = this.attributes.get()
+
+        var speedValue = (5.0 * max(attributes.speedStrain / 0.0675, 1.0) - 4.0).pow(3) / 100_000.0
+
+        // Longer maps are worth more
+        val lenBonus = 0.95 + 0.4 * min(totalHits / 2000.0, 1.0) +
+                (if(totalHits > 2000.0) 1.0 else 0.0) * 0.5 * log10(totalHits / 2000.0)
+        speedValue *= lenBonus
+
+        // Penalize misses
+        if (this.nMisses > 0) {
+            speedValue *= 0.97 * (1.0 - (this.nMisses / totalHits).pow(0.775)).pow(this.nMisses).pow(0.875)
+        }
+
+        // Combo scaling
+        this.combo.filter { attributes.maxCombo > 0 }.ifPresent { combo ->
+            speedValue *= min((combo.toDouble() / attributes.maxCombo.toDouble()).pow(0.8), 1.0)
+        }
+
+        // AR bonus
+        if (attributes.approachRate > 10.33) {
+            val approachRateFactor = 0.4 * (attributes.approachRate - 10.33)
+            speedValue *= 1.0 + min(approachRateFactor, approachRateFactor * totalHits / 1000.0)
+        }
+
+        // HD bonus
+        if (this.mods.hd()) {
+            speedValue *= 1.0 + 0.04 * (12.0 - attributes.approachRate)
+        }
+
+        // Scaling the speed value with accuracy and OD
+        val overallDifficultyFactor = 0.95 + attributes.overallDifficulty.pow(2) / 750.0
+        val accuracyFactor = this.acc.get().pow((14.5 - max(attributes.overallDifficulty,8.0)) / 2.0)
+        speedValue *= overallDifficultyFactor * accuracyFactor
+
+        // Penalize n50s
+        speedValue *= 0.98.pow(max(this.n50.orElse(0).toDouble() - totalHits / 500.0, 0.0))
+
+        return speedValue
+    }
+
+    private fun calculateAccuracy(totalHits: Double) : Double {
+        val attributes = this.attributes.get()
+        val nCircles = attributes.nCircles.toDouble()
+        val n300 = this.n300.orElse(0).toDouble()
+        val n100 = this.n100.orElse(0).toDouble()
+        val n50 = this.n50.orElse(0).toDouble()
+
+        val betterAccPercentage = (if (nCircles > 0.0) 1.0 else 0.0) *
+            max(((n300 - (totalHits - nCircles)) * 6.0 + n100 * 2.0 + n50) / (nCircles * 6.0), 0.0)
+
+        var accValue = 1.52163.pow(attributes.overallDifficulty) * betterAccPercentage.pow(24) * 2.83
+
+        // Bonus for many hitcircles
+        accValue *= min((nCircles / 1000.0).pow(0.3), 1.15)
+
+        // HD bonus
+        if (this.mods.hd()) accValue *= 1.08
+
+        // FL bonus
+        if (this.mods.fl()) accValue *= 1.02
+
+        return accValue
+    }
+
+    fun calculate() : PPResult {
         if(this.attributes.isEmpty) {
-            this.attributes = Optional.of(beatmap.stars(ModCombination(this.mods), this.passedObjects))
+            this.attributes = Optional.of(beatmap.stars(this.mods, this.passedObjects))
         }
         assertHitResults()
 
+        val totalHits = this.totalHits.toDouble()
+        var multiplier = 1.12
+
+        // NF penalty
+        if (this.mods.nf()) {
+            multiplier *= max(1.0 - 0.02 * this.nMisses.toDouble(), 0.9)
+        }
+
+        // SO penalty
+        if (this.mods.so()) {
+            multiplier *= 1.0 - (this.attributes.get().nSpinners.toDouble() / totalHits).pow(0.85)
+        }
+
+        val aimValue = calculateAim(totalHits)
+        val speedValue = calculateSpeed(totalHits)
+        val accValue = calculateAccuracy(totalHits)
+
+        val pp = (aimValue.pow(1.1) + speedValue.pow(1.1) + accValue.pow(1.1)).pow(1.0 / 1.1) * multiplier
+
+        return PPResult(
+            total = pp,
+            aim = aimValue,
+            speed = speedValue,
+            accuracy = accValue
+        )
     }
 
     companion object {
@@ -145,3 +297,10 @@ class PPCalculator private constructor(
     }
 
 }
+
+data class PPResult(
+    val total: Double,
+    val aim: Double,
+    val speed: Double,
+    val accuracy: Double
+)
