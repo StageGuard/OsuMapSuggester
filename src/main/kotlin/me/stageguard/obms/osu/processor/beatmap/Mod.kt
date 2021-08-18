08c8cbc6c01e56f1c9a076335e9ba5dbdbc98806
StageGuard <1355416608@qq.com>
2021-08-18 23:46:43 +0800

    add toList for ModCombination

--- src/main/kotlin/me/stageguard/obms/osu/processor/beatmap/Mod.kt
+++ src/main/kotlin/me/stageguard/obms/osu/processor/beatmap/Mod.kt
@@ -31,6 +31,23 @@ class ModCombination private constructor(private val value: Int) {
     fun ap() = value and Mod.Perfect.value > 0
     fun v2() = value and Mod.ScoreV2.value > 0
 
+    fun toList() = mutableListOf<Mod>().also {
+        if(nf()) it.add(Mod.NoFail)
+        if(ez()) it.add(Mod.Easy)
+        if(td()) it.add(Mod.TouchDevice)
+        if(hd()) it.add(Mod.Hidden)
+        if(hr()) it.add(Mod.HardRock)
+        if(dt()) it.add(Mod.DoubleTime)
+        if(nc()) it.add(Mod.NightCore)
+        if(rx()) it.add(Mod.Relax)
+        if(ht()) it.add(Mod.HalfTime)
+        if(fl()) it.add(Mod.Flashlight)
+        if(so()) it.add(Mod.SpunOut)
+        if(ap()) it.add(Mod.Autoplay)
+        if(v2()) it.add(Mod.ScoreV2)
+        if(it.isEmpty()) it.add(Mod.None)
+    }.toList()
+
     companion object {
         fun of(vararg mods: Mod) = ModCombination(
             if(mods.size == 1) mods.single().value else mods.drop(1).fold(mods.first().value) { r, v -> r or v.value }
@@ -43,22 +60,52 @@ class ModCombination private constructor(private val value: Int) {
 }
 
 sealed class Mod(val value: Int) {
-    object None : Mod(0)
-    object NoFail : Mod(1)
-    object Easy : Mod(2)
-    object TouchDevice : Mod(4)
-    object Hidden : Mod(8)
-    object HardRock : Mod(16)
-    object SuddenDeath : Mod(32)
-    object DoubleTime : Mod(64)
-    object Relax : Mod(128)
-    object HalfTime : Mod(256)
-    object NightCore : Mod(512)
-    object Flashlight : Mod(1024)
-    object Autoplay : Mod(2048)
-    object SpunOut : Mod(4096)
+    object None : Mod(0) {
+        override fun toString() = "NM"
+    }
+    object NoFail : Mod(1) {
+        override fun toString() = "NF"
+    }
+    object Easy : Mod(2) {
+        override fun toString() = "EZ"
+    }
+    object TouchDevice : Mod(4) {
+        override fun toString() = "TD"
+    }
+    object Hidden : Mod(8) {
+        override fun toString() = "HD"
+    }
+    object HardRock : Mod(16) {
+        override fun toString() = "HR"
+    }
+    object SuddenDeath : Mod(32) {
+        override fun toString() = "SD"
+    }
+    object DoubleTime : Mod(64) {
+        override fun toString() = "DT"
+    }
+    object Relax : Mod(128) {
+        override fun toString() = "RX"
+    }
+    object HalfTime : Mod(256) {
+        override fun toString() = "HT"
+    }
+    object NightCore : Mod(512) {
+        override fun toString() = "NC"
+    }
+    object Flashlight : Mod(1024) {
+        override fun toString() = "FL"
+    }
+    object Autoplay : Mod(2048) {
+        override fun toString() = "AP"
+    }
+    object SpunOut : Mod(4096) {
+        override fun toString() = "SO"
+    }
     object Relax2 : Mod(8192)
-    object Perfect : Mod(16384)
+    object Perfect : Mod(16384) {
+        override fun toString() = "PF"
+    }
     object Key4 : Mod(32768)
     object Key5 : Mod(65536)
     object Key6 : Mod(131072)
@@ -73,6 +120,8 @@ sealed class Mod(val value: Int) {
     object Key1 : Mod(67108864)
     object Key3 : Mod(134217728)
     object Key2 : Mod(268435456)
-    object ScoreV2 : Mod(536870912)
+    object ScoreV2 : Mod(536870912) {
+        override fun toString() = "V2"
+    }
     object Mirror : Mod(1073741824)
 }
