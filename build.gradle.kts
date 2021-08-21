cc6dd8babd8486014ec04c3a524576a7d7f8fb19
StageGuard <1355416608@qq.com>
2021-08-21 18:02:20 +0800

    update mirai to 2.7-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.7-RC-dev-3"
+    id("net.mamoe.mirai-console") version "2.7-RC"
 }
 
 group = "me.stageguard"
@@ -57,6 +57,7 @@ kotlin {
             languageSettings {
                 useExperimentalAnnotation("kotlin.RequiresOptIn")
                 useExperimentalAnnotation("kotlin.ExperimentalStdlibApi")
+                useExperimentalAnnotation("kotlin.contracts.ExperimentalContracts")
             }
         }
     }
