5a6b8b59757792fdc8d0f78f7451c8441298b2d0
StageGuard <1355416608@qq.com>
2021-09-06 10:48:28 +0800

    run in single thread

--- src/main/kotlin/me/stageguard/obms/bot/coroutineDispatchers.kt
+++ src/main/kotlin/me/stageguard/obms/bot/coroutineDispatchers.kt
@@ -9,26 +9,23 @@ import java.util.concurrent.Executors
 
 val THREAD_LOGGER = System.getProperty("me.stageguard.obms.thread_logger", false.toString()).toBoolean()
 
-private val graphicProcessorDispatcherCounter = atomic(0)
-val graphicProcessorDispatcher = Executors.newFixedThreadPool(5) { runnable ->
+val graphicProcessorDispatcher = Executors.newSingleThreadExecutor { runnable ->
     Thread(runnable).also {
-        it.name = "Graphics Processor #${graphicProcessorDispatcherCounter.getAndIncrement()}"
+        it.name = "OBMS Graphics"
         if(THREAD_LOGGER) OsuMapSuggester.logger.debug { "New graphics thread: $it" }
     }
 }.asCoroutineDispatcher()
 
-private val calculatorProcessorDispatcherCounter = atomic(0)
-val calculatorProcessorDispatcher = Executors.newFixedThreadPool(5) { runnable ->
+val calculatorProcessorDispatcher = Executors.newSingleThreadExecutor { runnable ->
     Thread(runnable).also {
-        it.name = "Calculator Processor #${calculatorProcessorDispatcherCounter.getAndIncrement()}"
+        it.name = "OBMS Calculator"
         if(THREAD_LOGGER) OsuMapSuggester.logger.debug { "New calculator thread: $it" }
     }
 }.asCoroutineDispatcher()
 
-private val networkProcessorDispatcherCounter = atomic(0)
-val networkProcessorDispatcher = Executors.newFixedThreadPool(10) { runnable ->
+val networkProcessorDispatcher = Executors.newSingleThreadExecutor { runnable ->
     Thread(runnable).also {
-        it.name = "Network Processor #${networkProcessorDispatcherCounter.getAndIncrement()}"
+        it.name = "OBMS Network"
         if(THREAD_LOGGER) OsuMapSuggester.logger.debug { "New network thread: $it" }
     }
 }.asCoroutineDispatcher()
