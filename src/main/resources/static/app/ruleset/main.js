11add63d41368544d2dfc647013dec0be1a8a3a4
StageGuard <1355416608@qq.com>
2021-10-06 11:31:26 +0800

    optimize toast

--- src/main/resources/static/app/ruleset/main.js
+++ src/main/resources/static/app/ruleset/main.js
@@ -4,6 +4,11 @@ const mainApp = Vue.createApp({
 
     template: `
         <!-- Top-right toast -->
+        <!--
+             data-mdb-width="535px" 
+            style="width: 535px; top: 10px; right: 10px; bottom: unset; left: unset; transform: unset;"
+            :style="{ display: toast.displayShowing ? 'block' : 'none' }"
+        -->
         <div 
             class="alert fade alert-fixed shadow-3" :class="{ 
                 'show': toast.classShowing,
@@ -12,13 +17,14 @@ const mainApp = Vue.createApp({
                 'alert-danger': toast.type === 2,
                 'alert-info': toast.type !== 0 && toast.type !== 1 && toast.type !== 2,
             }"
-            id="alert-primary" role="alert" 
-            data-mdb-color="primary" data-mdb-position="top-right" 
-            data-mdb-stacking="true" data-mdb-width="535px" 
-            data-mdb-append-to-body="true" data-mdb-hidden="true" 
-            data-mdb-autohide="true" data-mdb-delay="2000" 
-            style="width: 535px; top: 10px; right: 10px; bottom: unset; left: unset; transform: unset;"
-            :style="{ display: toast.displayShowing ? 'block' : 'none' }"
+            id="global-alert" role="alert"
+            aria-live="assertive" aria-atomic="true" :data-mdb-width="alertWidth" 
+            data-mdb-color="primary"
+            data-mdb-stacking="true" data-mdb-append-to-body="true"
+            data-mdb-hidden="true" data-mdb-autohide="true"
+            :data-mdb-delay="toast.duration"
+            style="top: 10px; right: 10px;"
+            :style = "{ width: alertWidth }"
             v-html="toast.content"
         ></div>
         
@@ -39,17 +45,21 @@ const mainApp = Vue.createApp({
     data() {
         return {
             toast: {
-                classShowing: false,
-                displayShowing: false,
                 type: 0,
-                content: ""
+                content: "",
+                duration: 3500
             },
             showAuthorizationDialog: true,
             showRulesetEditor: false,
+            alertWidth: "582px",
             qq: Number(),
             osuId: Number(),
             osuName: String(),
         }
+    },
+
+    created() {
+        window.onresize = this._onWindowResize
     },
 
     methods: {
@@ -58,53 +68,54 @@ const mainApp = Vue.createApp({
             this.osuId = osuId;
             this.osuName = osuName;
 
-            this.showAuthorizationDialog = false
-            this.showRulesetEditor = true
+            this.showAuthorizationDialog = false;
+            this.showRulesetEditor = true;
+        },
+
+        _onWindowResize() {
+            let width = window.screen.width;
+
+            if(width >= 600) {
+                this.alertWidth = "582px";
+            } else {
+                this.alertWidth = (width - 20) + "px";
+            }
         },
 
         _toast(contentHTML, duration, type) {
             this.toast.type = type || 0;
             this.toast.content = contentHTML || "No content.";
-            this.toast.displayShowing = true;
-            // delay for animation
-            setTimeout(() => { this.toast.classShowing = true; }, 50);
+            this.toast.duration = duration || 3500;
 
-            new Promise(resolve => {
-                 setTimeout(() => {
-                     this.toast.classShowing = false;
-                     resolve();
-                 }, duration || 3500);
-            }).then(_ => {
-                setTimeout(() => {
-                    this.toast.displayShowing = false;
-                }, 250);
-            });
+            const globalToast = document.getElementById("global-alert")
+            const toast = new mdb.Toast(globalToast);
+            toast.show();
         },
 
         toastError(message, duration) {
             this._toast(
-                '<i class="fa fa-times-circle"></i> ' + message,
+                '<i class="fa fa-times-circle me-3"></i> ' + message,
                 duration || 8500, 2
             );
         },
 
         toastWarning(message, duration) {
             this._toast(
-                '<i class="fa fa-exclamation-triangle"></i> ' + message,
+                '<i class="fa fa-exclamation-triangle me-3"></i> ' + message,
                 duration || 4500, 1
             );
         },
 
         toastSuccess(message, duration) {
             this._toast(
-                '<i class="fa fa-check-circle"></i> ' + message,
+                '<i class="fa fa-check-circle me-3"></i> ' + message,
                 duration || 3000, 0
             );
         },
 
         toastInfo(message, duration) {
             this._toast(
-                '<i class="fa fa-info-circle"></i> ' + message,
+                '<i class="fa fa-info-circle me-3"></i> ' + message,
                 duration || 3000, 3
             );
         },
