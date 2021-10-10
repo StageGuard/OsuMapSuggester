b06116628b0b1c64971d1e2d96d42fc658d6f903
StageGuard <1355416608@qq.com>
2021-10-10 21:13:59 +0800

    show warning if failed to export resources

--- src/main/kotlin/me/stageguard/obms/utils/exportResources.kt
+++ src/main/kotlin/me/stageguard/obms/utils/exportResources.kt
@@ -4,35 +4,58 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.utils.info
+import net.mamoe.mirai.utils.warning
 import java.io.File
 import java.io.InputStream
 
 suspend fun KotlinPlugin.exportStaticResourcesToDataFolder() = withContext(Dispatchers.IO) {
-    val fonts = listOf("Torus-Bold.otf", "Torus-Regular.otf", "Torus-SemiBold.otf")
-    val mods = listOf("mod_dt.png", "mod_ez.png", "mod_fl.png", "mod_hd.png", "mod_hr.png", "mod_ht.png", "mod_nc.png", "mod_nf.png", "mod_nm.png", "mod_pf.png", "mod_sd.png", "mod_so.png")
-    val grades = listOf("grade_a.svg", "grade_b.svg", "grade_c.svg", "grade_d.svg", "grade_s.svg", "grade_sh.svg", "grade_x.svg", "grade_xh.svg", "grade_f.svg")
-    val hits = listOf("hit_good.png", "hit_great.png", "hit_meh.png", "hit_miss.png")
-    fonts.map { "/font/$it" }
-        .plus(mods.map { "/image/$it" })
-        .plus(grades.map { "/svg/$it" })
-        .plus(hits.map { "/image/$it" })
-        .toMutableList().also {
-            it.add("/image/background.png")
-            it.add("/svg/arrow_right.svg")
-            it.add("/svg/bpm.svg")
-            it.add("/svg/total_length.svg")
-            it.add("/image/avatar_guest.png")
-        }.forEach {
-            val inputStream = this@exportStaticResourcesToDataFolder::class.java.getResourceAsStream(it)
-            val outputFile = dataFolder.absolutePath + File.separator + "resources" + it
-            if(inputStream != InputStream.nullInputStream()) {
-                File(outputFile).run {
-                    File(parent).mkdirs()
-                    createNewFile()
-                    writeBytes(inputStream.readAllBytes())
-                }
+    listOf(
+        "/font/Torus-Bold.otf",
+        "/font/Torus-Regular.otf",
+        "/font/Torus-SemiBold.otf",
+        "/image/mod_dt.png",
+        "/image/mod_ez.png",
+        "/image/mod_fl.png",
+        "/image/mod_hd.png",
+        "/image/mod_hr.png",
+        "/image/mod_ht.png",
+        "/image/mod_nc.png",
+        "/image/mod_nf.png",
+        "/image/mod_nm.png",
+        "/image/mod_pf.png",
+        "/image/mod_sd.png",
+        "/image/mod_so.png",
+        "/svg/grade_a.svg",
+        "/svg/grade_b.svg",
+        "/svg/grade_c.svg",
+        "/svg/grade_d.svg",
+        "/svg/grade_s.svg",
+        "/svg/grade_sh.svg",
+        "/svg/grade_x.svg",
+        "/svg/grade_xh.svg",
+        "/svg/grade_f.svg",
+        "/image/hit_good.png",
+        "/image/hit_great.png",
+        "/image/hit_meh.png",
+        "/image/hit_miss.png",
+        "/image/background.png",
+        "/svg/arrow_right.svg",
+        "/svg/bpm.svg",
+        "/svg/total_length.svg",
+        "/image/avatar_guest.png"
+    ).forEach {
+        val inputStream = this@exportStaticResourcesToDataFolder::class.java.getResourceAsStream(it)
+        val outputFile = dataFolder.absolutePath + File.separator + "resources" + it
+        if(inputStream != null && inputStream != InputStream.nullInputStream()) {
+            File(outputFile).run {
+                File(parent).mkdirs()
+                createNewFile()
+                writeBytes(inputStream.readAllBytes())
             }
-            inputStream.close()
+        } else {
+            this@exportStaticResourcesToDataFolder.logger.warning { "Resource not found: $it" }
         }
+        inputStream?.close()
+    }
     this@exportStaticResourcesToDataFolder.logger.info { "Successfully exported static resources." }
 }
