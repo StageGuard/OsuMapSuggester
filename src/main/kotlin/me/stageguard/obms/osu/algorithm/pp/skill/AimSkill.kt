64d96223159e835b88af50057aa4ec5841e89c9c
StageGuard <1355416608@qq.com>
2021-11-10 21:51:06 +0800

    apply aim strain algorithm from https://github.com/ppy/osu/pull/14847/files

--- src/main/kotlin/me/stageguard/obms/osu/algorithm/pp/skill/AimSkill.kt
+++ src/main/kotlin/me/stageguard/obms/osu/algorithm/pp/skill/AimSkill.kt
@@ -1,47 +1,110 @@
 package me.stageguard.obms.osu.algorithm.pp.skill
 
-import me.stageguard.obms.osu.processor.beatmap.ModCombination
 import me.stageguard.obms.osu.algorithm.pp.DifficultyObject
 import me.stageguard.obms.osu.algorithm.pp.Skill
-import kotlin.math.max
-import kotlin.math.pow
-import kotlin.math.sin
-import kotlin.math.sqrt
+import me.stageguard.obms.osu.processor.beatmap.ModCombination
+import kotlin.math.*
 
 class AimSkill(mods: ModCombination) : Skill<DifficultyObject>(mods) {
-    private val AIM_SKILL_MULTIPLIER: Double = 26.25
+    private val AIM_SKILL_MULTIPLIER: Double = 23.25
     private val AIM_STRAIN_DECAY_BASE: Double = 0.15
 
-    private val AIM_ANGLE_BONUS_BEGIN: Double = Math.PI / 3.0
-    private val TIMING_THRESHOLD: Double = 107.0
+    private val WIDE_ANGLE_MULTIPLIER: Double = 1.5
+    private val ACUTE_ANGLE_MULTIPLIER: Double = 2.0
+    private val SLIDER_MULTIPLIER: Double = 1.5
+    private val VELOCITY_CHANGE_MULTIPLIER: Double = 0.75
 
     override val strainDecayBase: Double
         get() = AIM_STRAIN_DECAY_BASE
     override val skillMultiplier: Double
         get() = AIM_SKILL_MULTIPLIER
 
-    override fun strainValueOf(current: DifficultyObject): Double = if (current.base.isSpinner) { 0.0 } else {
-        var result = 0.0
+    override fun strainValueOf(current: DifficultyObject): Double = if (
+        current.base.isSpinner || !isPrevQueueFull || prevObj?.base?.isSpinner == true
+    ) { 0.0 } else {
+        var currVelocity = current.jumpDistance / current.strainTime
+        if (prevObj?.base?.isSlider == true) {
+            val movementVelocity = current.movementDistance / current.movementTime
+            val travelVelocity = current.travelDistance / current.travelTime
 
-        current.prev.ifPresent { (prevJumpDist, prevStrainTime) ->
-            current.angle.filter { it > AIM_ANGLE_BONUS_BEGIN }.ifPresent { angle ->
-                val scale = 90.0
+            currVelocity = max(currVelocity, movementVelocity * travelVelocity)
+        }
 
-                val angleBonus = sqrt(
-                    sin(angle - AIM_ANGLE_BONUS_BEGIN).pow(2) *
-                            max(prevJumpDist - scale, 0.0) *
-                            max(current.movementDistance - scale, 0.0)
-                )
+        var prevVelocity = prevObj!!.jumpDistance / prevObj!!.strainTime
+        if (prevPrevObj?.base?.isSlider == true) {
+            val movementVelocity = prevObj!!.movementDistance / prevObj!!.movementTime
+            val travelVelocity = prevObj!!.travelDistance / prevObj!!.travelTime
 
-                result = 1.4 * applyDiminishingExp(max(angleBonus, 0.0)) / max(TIMING_THRESHOLD, prevStrainTime)
+            prevVelocity = max(prevVelocity, movementVelocity * travelVelocity)
+        }
+
+        var wideAngleBonus = 0.0
+        var acuteAngleBonus = 0.0
+        var sliderBonus = 0.0
+        var velocityChangeBonus = 0.0
+
+        var aimStrain = currVelocity
+
+        if (max(current.strainTime, prevObj!!.strainTime) < 1.25 * min(current.strainTime, prevObj!!.strainTime)) {
+            if (current.angle.isPresent && prevObj!!.angle.isPresent && prevPrevObj!!.angle.isPresent) {
+                val currAngle: Double = current.angle.get()
+                val lastAngle: Double = prevObj!!.angle.get()
+                val lastLastAngle: Double = prevPrevObj!!.angle.get()
+
+                val angleBonus: Double = min(currVelocity, prevVelocity)
+                wideAngleBonus = calcWideAngleBonus(currAngle)
+                acuteAngleBonus = calcAcuteAngleBonus(currAngle)
+                if (current.strainTime > 100) {
+                    acuteAngleBonus = 0.0
+                } else {
+                    acuteAngleBonus *= (calcAcuteAngleBonus(lastAngle) * min(angleBonus, 125 / current.strainTime)
+                        * sin(Math.PI / 2 * min(1.0, (100 - current.strainTime) / 25)).pow(2)
+                        * sin(Math.PI / 2 * (current.jumpDistance.coerceIn(50.0, 100.0) - 50.0) / 50.0).pow(2))
+                }
+
+                wideAngleBonus *= angleBonus * (1 - min(wideAngleBonus, calcWideAngleBonus(lastAngle).pow(3)))
+                acuteAngleBonus *= 0.5 + 0.5 * (1 - min(acuteAngleBonus, calcAcuteAngleBonus(lastLastAngle).pow(3)))
             }
         }
 
-        val jumpDistExp = applyDiminishingExp(current.movementDistance)
-        val travelDistExp = applyDiminishingExp(current.travelDistance)
+        if (max(prevVelocity, currVelocity) != 0.0) {
+            prevVelocity = (prevObj!!.jumpDistance + prevObj!!.travelDistance) / prevObj!!.strainTime
+            currVelocity = (current.jumpDistance + current.travelDistance) / current.strainTime
 
-        val distExp = jumpDistExp + travelDistExp + sqrt(travelDistExp * jumpDistExp)
+            val distRatio: Double = sin(
+                Math.PI / 2 * abs(prevVelocity - currVelocity) / max(prevVelocity, currVelocity)
+            ).pow(2)
 
-        max(result + distExp / max(current.movementTime, TIMING_THRESHOLD), distExp / current.movementTime)
+            val overlapVelocityBuff: Double = min(
+                125 / min(current.strainTime, prevObj!!.strainTime), abs(prevVelocity - currVelocity)
+            )
+
+            val nonOverlapVelocityBuff: Double = (abs(prevVelocity - currVelocity)
+                    * sin(Math.PI / 2 * min(1.0, min(current.jumpDistance, prevObj!!.jumpDistance) / 100.0)
+            ).pow(2))
+
+            velocityChangeBonus = max(overlapVelocityBuff, nonOverlapVelocityBuff) * distRatio
+
+            velocityChangeBonus *= (min(current.strainTime, prevObj!!.strainTime) / max(
+                current.strainTime, prevObj!!.strainTime
+            )).pow(2)
+        }
+
+        if (current.travelTime != 0.0) {
+            sliderBonus = current.travelDistance / current.travelTime
+        }
+
+        aimStrain += max(
+            acuteAngleBonus * ACUTE_ANGLE_MULTIPLIER,
+            wideAngleBonus * WIDE_ANGLE_MULTIPLIER + velocityChangeBonus * VELOCITY_CHANGE_MULTIPLIER
+        )
+        aimStrain += sliderBonus * SLIDER_MULTIPLIER
+
+        aimStrain
     }
+    
+    private fun calcWideAngleBonus(angle: Double) =
+        sin(3.0 / 4 * ((5.0 / 6 * Math.PI).coerceAtMost((Math.PI / 6).coerceAtLeast(angle)) - Math.PI / 6)).pow(2.0)
+
+    private fun calcAcuteAngleBonus(angle: Double) = 1 - calcWideAngleBonus(angle)
 }
